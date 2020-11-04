0247f595777beea8a4c86fe827ad45baf073dfde
6hha <73243561+6hha@users.noreply.github.com>
2020-11-04 05:35:22 +0300

    Update Aslopic.lua

--- Aslopic.lua
+++ Aslopic.lua
@@ -1,4 +1,4 @@
-[-- 
+[ -- 
    _____         .__                .__        
   /  _  \   _____|  |   ____ ______ |__| ____  
  /  /_\  \ /  ___/  |  /  _ \\____ \|  |/ ___\ 
@@ -9,6 +9,7 @@
         Owner: xlomished (no nut november)#7393
         Developer: m5#0001
         Discord Invite: https://discord.gg/URr9XQVrdd
---]
+-- ]
+
 if game.Id == 155615604 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/6hha/Aslopic-Hub/main/Prison%20Life.lua"))()
