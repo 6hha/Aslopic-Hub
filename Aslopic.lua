f6fe158a5f3d54fa860a26d68575667ead328b74
6hha <73243561+6hha@users.noreply.github.com>
2020-11-04 05:29:16 +0300

    Update and rename README.md to Aslopic.lua

---
+++ Aslopic.lua
@@ -0,0 +1,14 @@
+[-- 
+   _____         .__                .__        
+  /  _  \   _____|  |   ____ ______ |__| ____  
+ /  /_\  \ /  ___/  |  /  _ \\____ \|  |/ ___\ 
+/    |    \\___ \|  |_(  <_> )  |_> >  \  \___ 
+\____|__  /____  >____/\____/|   __/|__|\___  >
+        \/     \/            |__|           \/ 
+        
+        Owner: xlomished (no nut november)#7393
+        Developer: m5#0001
+        Discord Invite: https://discord.gg/URr9XQVrdd
+--]
+if game.Id == 155615604 then
+https://raw.githubusercontent.com/6hha/Aslopic-Hub/main/Prison%20Life.lua
--- README.md
+++
@@ -1,1 +0,0 @@
-# Aslopic-Hub
